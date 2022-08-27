49942b2a099359efdb44d00a0345a57d4529d2d5
BAGUS BUDI SATOTO <87259393+bagussatoto@users.noreply.github.com>
2022-08-27 10:00:21 +0700

    commit

--- src/components/Nearby.vue
+++ src/components/Nearby.vue
@@ -3,7 +3,7 @@
       <div class="show flex justify-between items-end">
          <span class="text-blue-500 text-xs font-medium">
             Nearby Doctor <br />
-            <small class="text-gray-400 text-xs font-medium" >Los Angeles, California</small>
+            <small class="text-gray-400 text-xs font-medium" >Yogyakarta, Indonesia</small>
          </span>
          <span class="text-gray-400 text-xs font-medium">
             See all
@@ -13,14 +13,14 @@
       <template v-for="card in 8" :key="card">
          <div class="show w-full bg-subsecondary mt-3 flex justify-between gap-2 items-end p-3 rounded-lg">
             <div>
-               <img src="/profile.jpg" class="rounded-lg" width="80" />
+               <img src="/indra.png" class="rounded-lg" width="80" />
             </div>
             <div class="text-blue-500 flex flex-wrap">
-               <strong class="text-xs block w-full">Dr. Amanda Kirana</strong>
-               <p class="text-xxs text-gray-400">Psychologist</p>
+               <strong class="text-xs block w-full">Dr. Indrawan</strong>
+               <p class="text-xxs text-gray-400">Surgeon</p>
             </div>
             <small class="text-xxs text-gray-400 font-medium">
-               0.5km
+               10 km
             </small>
          </div>
       </template>
